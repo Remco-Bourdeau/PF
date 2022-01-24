d490ae1c675233e796144392d4fcd69faee92023
cursist <cursist@DEVELOPMENT>
2022-01-24 09:14:27 +0100

    Add project files.

---
+++ CSharpCorePFCursus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpCorePFCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Asterix");
+            Console.Write(" en Obelix");
+            Console.ReadKey();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.SetCursorPosition(10, 2);
+            Console.WriteLine("Suske");
+            Console.SetCursorPosition(25,6);
+            Console.WriteLine("Wiske");
+        }
+    }
+}
